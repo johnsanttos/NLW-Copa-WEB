5cee57dea0a3ba48ce81c3b618ec6c71b283409f
John Santos <jhow.mito11@gmail.com>
2023-03-10 16:36:14 -0300

    layout do site completo

--- copa-web/src/pages/index.tsx
+++ copa-web/src/pages/index.tsx
@@ -4,22 +4,72 @@
 import Image from 'next/image'
 import logoImg from '../assets/logo.svg'
 import appPreviewImg from '../assets/app-nlw-copa-preview.png'
+import userAvatarExampleImg from '../assets/users-avatar-example.png'
+import iconCheckImg from '../assets/icon-check.svg'
+
+
+
+
+
+
 export default function Home() {
 
   return (
-<div>
-  <main>
-  <Image src={logoImg}  alt='Dois celulares exibindo uma prévia da aplicação'
-quality={100}
-/>
-<h1>
-Crie seu próprio bolão da copa e compartilhe entre amigos!
-</h1>
-  </main>
-<Image src={appPreviewImg}  alt='Dois celulares exibindo uma prévia da aplicação'
-quality={100}
-/>
-</div>
+    <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28"  >
+      <main>
+
+        <Image src={logoImg} alt='Dois celulares exibindo uma prévia da aplicação'
+        />
+        <h1 className='mt-14 text-white text-5xl font-bold leading-tight'>
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+
+        <div className='mt-10 flex items-center gap-2'>
+          <Image src={userAvatarExampleImg} alt=''
+          />
+
+          <strong className='text-gray-100 text-xl'> <span className='text-ignite-500'> + 12.592 </span> pessoas já estão usando</strong>
+        </div>
+        <form className='mt-10 flex gap-2' >
+
+          <input 
+          className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+          type="text" required placeholder='Qual o nome do seu bolão' />
+
+          <button 
+          className='bg-yellow-500  px-6 py-4 rounded font-bold text-sm uppercase hover:bg-yellow-700'
+          type='submit'> Criar meu bolão</button>
+        </form>
+
+        <p className="text-gray-300 mt-4 text-sm leading-relaxed">
+          Após criar seu bolão, você receberá um código único que poderá usar para convidar outras pessoas 🚀
+        </p>
+
+        <div className='mt-10 pt-10 border-t items-center border-gray-600 flex justify-between text-gray-100'>
+          <div className='flex items-center gap-6'>
+            <Image src={iconCheckImg} alt='' />
+            <div className=' flex flex-col'>
+              <span className='font-bold'> +2.034 </span>
+              <span> Bolões criados </span>
+
+            </div>
+          </div>
+          <div className='w-px h-14 bg-gray-600' />
+
+          <div className='flex items-center gap-6'> 
+            <Image src={iconCheckImg} alt='' />
+            <div  className=' flex flex-col'>
+              <span className='font-bold'> +192.847 </span>
+              <span> Palpites enviados </span>
+            </div>
+          </div>
+        </div>
+
+      </main>
+      <Image src={appPreviewImg} alt='Dois celulares exibindo uma prévia da aplicação'
+        quality={100}
+      />
+    </div>
   )
 }
 
--- copa-web/tailwind.config.js
+++ copa-web/tailwind.config.js
@@ -9,9 +9,27 @@ module.exports = {
         sans: 'Roboto, sans-serif'
       },
 
+      backgroundImage: {
+        app: 'url(/app-bg.png)'
+      },
+
       colors:{
+        ignite:{
+          500:'#129e57'
+        },
+
+       
+
+        yellow:{
+          500: '#f7dd43',
+          700:'#e5cd3d'
+        },
         gray:{
-          900: '#121214'
+          100:'#e1e1e6',
+          300:'#8d8d99',
+          600:'#323238',
+          800:'#202024',
+          900: '#121214'          
         }
       }
     },
